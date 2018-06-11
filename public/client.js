df21a9173e846b2cd7bed0e76fec86003b510455
Glitch (project-tracker12214) <none>
2018-06-11 20:13:07 +0000

    🍮💽 Checkpoint ./public/client.js:1174538/1153 ./public/style.css:1174538/159

--- public/client.js
+++ public/client.js
@@ -19,7 +19,28 @@
     {
     console.log('hello world :o');
     console.log(data);
-    
+    var issues= [];
+            data.forEach(function(ele) {
+              console.log(ele);
+              var openstatus;
+              (ele.open) ? openstatus = 'open' : openstatus = 'closed';
+              var single = [
+                '<div class="issue '+openstatus+'">',
+                '<p class="id">id: '+ele.id1+'</p>',
+                '<h3>'+ele.title+' -  ('+openstatus+')</h3>',
+                '<br>',
+                '<p>'+ele.text+'</p>',
+                '<p>'+ele.status_text+'</p>',
+                '<br>',
+                '<p class="id"><b>Created by:</b> '+ele.created_by+'  <b>Assigned to:</b> '+ele.assigned_to,
+                '<p class="id"><b>Created on:</b> '+ele.created_on+'  <b>Last updated:</b> '+ele.updated_on,
+                '<br><a href="#" class="closeIssue" id="'+ele._id+'">close?</a> <a href="#" class="deleteIssue" id="'+ele._id+'">delete?</a>',
+                '</div>'
+                
+              ];
+              issues.push(single.join(''));
+            });
+          $('#output').html(issues.join(''));
     },
     error: function(err){
     console.log("hello");
