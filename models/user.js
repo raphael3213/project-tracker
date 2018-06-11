06229aeadbd82ed1fa632436bdab810c5b3ff2d1
Glitch (rain-scissor) <none>
2018-06-11 15:02:56 +0000

    🎳🏀 Checkpoint ./models/user.js:1174538/294

---
+++ models/user.js
@@ -0,0 +1,18 @@
+var mongoose=require('mongoose');
+
+var userScheme=mongoose.Schema({
+
+  title:String,
+  text:String,
+  created_on:Date,
+  updated_on:Date,
+  created_by:String,
+  assigned_to:String,
+  open:Boolean,
+  status_text:String
+
+
+
+})
+
+
