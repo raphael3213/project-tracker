4927afb0c83068d5793fd5e94de094beecdc78c9
Glitch (project-tracker12214) <none>
2018-06-11 20:22:43 +0000

    Exporting to raphael3213/project-tracker

--- public/client.js
+++ public/client.js
@@ -21,19 +21,23 @@
     console.log(data);
     var issues= [];
             data.forEach(function(ele) {
-              console.log(ele);
-              var openstatus;
-              (ele.open) ? openstatus = 'open' : openstatus = 'closed';
+              var up="";
+              
+              if(ele.updated_on){up=ele.updated_on}
+              var openstatus=ele.open;
+              
               var single = [
                 '<div class="issue '+openstatus+'">',
                 '<p class="id">id: '+ele.id1+'</p>',
                 '<h3>'+ele.title+' -  ('+openstatus+')</h3>',
                 '<br>',
+                '<h4>Description</h4>',
                 '<p>'+ele.text+'</p>',
+                '<h4>Status</h4>',
                 '<p>'+ele.status_text+'</p>',
                 '<br>',
                 '<p class="id"><b>Created by:</b> '+ele.created_by+'  <b>Assigned to:</b> '+ele.assigned_to,
-                '<p class="id"><b>Created on:</b> '+ele.created_on+'  <b>Last updated:</b> '+ele.updated_on,
+                '<p class="id"><b>Created on:</b> '+ele.created_on+'  <b>Last updated:</b> '+up,
                 '<br><a href="#" class="closeIssue" id="'+ele._id+'">close?</a> <a href="#" class="deleteIssue" id="'+ele._id+'">delete?</a>',
                 '</div>'
                 
