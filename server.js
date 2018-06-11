2e3d0b35ba270ea07a59ffe23fc8d84313667d00
Glitch (rain-scissor) <none>
2018-06-11 16:04:31 +0000

    🏢🔖 Checkpoint ./server.js:1174538/453

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 
 var express = require('express');
 var app = express();
-
+var mongoose=require('mongoose')
+var user=require('./models/user.js');
 app.use(express.static('public'));
+var bp=require('body-parser');
 
-// http://expressjs.com/en/starter/basic-routing.html
+app.use(bp.json());
+app.use(bp.urlencoded({extended:false}));
+
+mongoose.connect(process.env.MONG_URI,function(err){
+
+  if(err){console.err}
+  
+console.log("connected to database");
+
+});
+
+app.get
+
+
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
